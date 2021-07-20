def875acfb60b6d812b23268decbbcee24b303dd
Reddys-S <82322474+Reddys-S@users.noreply.github.com>
2021-07-20 15:28:43 +0300

    Reddys-S
    Reddys-S

--- js/script.js
+++ js/script.js
@@ -1,17 +1,34 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+// для сайдпанель чтобы вылезала и закрывалась на крестик
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+
+$('.sertificat__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    infinite: false,
+    focusOnSelect: true,
+    cssEase: 'linear',
+    swipe: true,
+    prevArrow: '<img src="img/arrow.svg" class="slick-prev">',
+    nextArrow: '<img src="img/arrow.svg" class="slick-next">',
+});
+
+$("a[href^='#']").on("click", function (e) {
+    e.preventDefault();
+    var anchor = $(this).attr('href');
+    $('html, body').stop().animate({
+    scrollTop: $(anchor).offset().top
+    }, 800);
     });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
